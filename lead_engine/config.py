a00323e0157eefb6b09bdb71a5d981934402808f
Katelyn <tanner8922@gmail.com>
2026-08-27 00:45:40 -0500

    Refactor LeadEngineConfig to use DEFAULT_BATCH_SIZE

--- lead_engine/config.py
+++ lead_engine/config.py
@@ -1,5 +1,9 @@
 import os
 from dataclasses import dataclass
+from pathlib import Path
+
+
+DEFAULT_BATCH_SIZE = 50
 
 
 @dataclass
@@ -14,46 +18,63 @@ class LeadEngineConfig:
     database_dir: str = "data"
     airtable_base_id: str = ""
     airtable_table: str = "Lead Radar"
-    batch_size: int = 50
+    batch_size: int = DEFAULT_BATCH_SIZE
     sync_enabled: bool = True
 
     @classmethod
     def from_environment(cls):
-        return cls(
-            database_dir=os.getenv(
+        database_dir = os.getenv(
+            "LEAD_ENGINE_DATA_DIR",
+            os.getenv(
                 "LEAD_ENGINE_DATABASE_DIR",
                 "data",
             ),
+        )
+
+        airtable_table = os.getenv(
+            "AIRTABLE_LEAD_TABLE",
+            os.getenv(
+                "AIRTABLE_TABLE",
+                "Lead Radar",
+            ),
+        )
+
+        raw_batch_size = os.getenv(
+            "LEAD_ENGINE_BATCH_SIZE",
+            str(DEFAULT_BATCH_SIZE),
+        )
+
+        try:
+            batch_size = int(raw_batch_size)
+        except (TypeError, ValueError):
+            batch_size = DEFAULT_BATCH_SIZE
+
+        if batch_size <= 0:
+            batch_size = DEFAULT_BATCH_SIZE
+
+        sync_enabled = os.getenv(
+            "LEAD_ENGINE_SYNC_ENABLED",
+            "true",
+        ).strip().lower() in {
+            "1",
+            "true",
+            "yes",
+            "on",
+        }
+
+        return cls(
+            database_dir=database_dir,
             airtable_base_id=os.getenv(
                 "AIRTABLE_BASE_ID",
                 "",
             ),
-            airtable_table=os.getenv(
-                "AIRTABLE_TABLE",
-                "Lead Radar",
-            ),
-            batch_size=int(
-                os.getenv(
-                    "LEAD_ENGINE_BATCH_SIZE",
-                    "50",
-                )
-            ),
-            sync_enabled=os.getenv(
-                "LEAD_ENGINE_SYNC_ENABLED",
-                "true",
-            ).lower()
-            in {
-                "1",
-                "true",
-                "yes",
-                "on",
-            },
+            airtable_table=airtable_table,
+            batch_size=batch_size,
+            sync_enabled=sync_enabled,
         )
 
     @property
-    def database_path(self):
-        from pathlib import Path
-
+    def database_path(self) -> Path:
         return (
             Path(self.database_dir)
             / "leads.sqlite3"
