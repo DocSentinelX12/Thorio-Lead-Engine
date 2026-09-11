75f9ec8bed3ad23573213af6da4b74465b34bb14
Katelyn <tanner8922@gmail.com>
2026-09-11 04:52:11 -0500

    Strengthen source override isolation tests

--- lead_engine/test_source_overrides.py
+++ lead_engine/test_source_overrides.py
@@ -1,11 +1,33 @@
-from unittest.mock import patch
-
 from .source_registry import _load_free_source_catalog
 from .source_overrides import _SOURCE_OVERRIDES, _apply_overrides
 
 
+_ACTIVE_SOURCES = {
+    "Himalayas",
+    "Jobicy",
+    "Remote OK",
+    "Airbnb",
+    "Anthropic",
+    "Airtable",
+    "Asana",
+    "Brex",
+    "Cloudflare",
+    "Coinbase",
+    "Datadog",
+    "Discord",
+    "Dropbox",
+    "Figma",
+    "GitLab",
+    "Instacart",
+    "Lyft",
+    "Netlify",
+    "Stripe",
+}
+
+
 def test_only_explicit_broken_sources_are_overridden():
     assert set(_SOURCE_OVERRIDES) == {"The Muse"}
+    assert not (set(_SOURCE_OVERRIDES) & _ACTIVE_SOURCES)
 
 
 def test_the_muse_uses_current_api_page_one():
@@ -26,12 +48,16 @@ def test_the_muse_uses_current_api_page_one():
     assert muse.url_field == "refs.landing_page"
 
 
-def test_healthy_definition_is_unchanged():
+def test_every_active_source_definition_is_unchanged():
     catalog = _load_free_source_catalog()
-    healthy = next(
-        definition
+    definitions = {
+        definition.name: definition
         for definition in catalog
-        if definition.name == "Stripe"
+    }
+
+    active_definitions = tuple(
+        definitions[name]
+        for name in sorted(_ACTIVE_SOURCES)
     )
-    overridden = _apply_overrides((healthy,))[0]
-    assert overridden == healthy
+
+    assert _apply_overrides(active_definitions) == active_definitions
