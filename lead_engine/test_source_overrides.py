02cb6cb4f1c1329db4de522f592f0bec7415928a
Katelyn <tanner8922@gmail.com>
2026-09-15 02:57:36 -0500

    Test Landing Jobs public feed override

--- lead_engine/test_source_overrides.py
+++ lead_engine/test_source_overrides.py
@@ -106,6 +106,22 @@ def test_jobicy_uses_public_rss_fallback():
     assert jobicy.max_requests == 1
 
 
+def test_landing_jobs_uses_public_rss_feed():
+    catalog = _load_free_source_catalog()
+    landing_jobs = next(
+        definition
+        for definition in _apply_overrides(tuple(catalog))
+        if definition.name == "Landing Jobs"
+    )
+
+    assert landing_jobs.collector_type == "rss"
+    assert landing_jobs.url == "https://landing.jobs/feed"
+    assert landing_jobs.pagination_type == "none"
+    assert landing_jobs.max_pages == 1
+    assert landing_jobs.max_requests == 1
+    assert landing_jobs.max_records == 55
+
+
 def test_current_api_corrections_remove_stale_explicit_page_parameters():
     catalog = _load_free_source_catalog()
     definitions = {definition.name: definition for definition in catalog}
