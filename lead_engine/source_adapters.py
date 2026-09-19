a8cda3fedb1a074073ee5806f71f5dc7f3eb587c
Katelyn <tanner8922@gmail.com>
2026-09-18 23:59:23 -0500

    fix: place pagination progress update in configured adapter

--- lead_engine/source_adapters.py
+++ lead_engine/source_adapters.py
@@ -745,21 +745,6 @@ class JsonSourceAdapter:
 
             normalized = []
 
-            if pagination_type == "page":
-                final_checkpoint = str(
-                    current_page + 1
-                )
-            elif pagination_type == "offset":
-                step = (
-                    definition.offset_step
-                    or definition.page_limit
-                    or len(records)
-                    or 1
-                )
-                final_checkpoint = str(
-                    current_offset + step
-                )
-
             for item in records:
                 record = normalize_job_record(
                     item,
@@ -796,6 +781,21 @@ class JsonSourceAdapter:
             )
 
             normalized = []
+
+            if pagination_type == "page":
+                final_checkpoint = str(
+                    current_page + 1
+                )
+            elif pagination_type == "offset":
+                step = (
+                    definition.offset_step
+                    or definition.page_limit
+                    or len(records)
+                    or 1
+                )
+                final_checkpoint = str(
+                    current_offset + step
+                )
 
             for item in records:
                 record = normalize_job_record(
