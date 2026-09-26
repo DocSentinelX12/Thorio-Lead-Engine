cd7d47fbeaf71733f67b447381903076c2c022c5
Katelyn <tanner8922@gmail.com>
2026-09-26 00:06:21 -0500

    feat: add Astrivon delivery partition

--- lead_engine/partner_delivery.py
+++ lead_engine/partner_delivery.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List
 from .delivery_gate import evaluate_delivery_gate
 
 
-PARTNER_ROUTES = ("Shiftr", "Paxus", "Thorio")
+PARTNER_ROUTES = ("Shiftr", "Paxus", "Thorio", "Astrivon Labs")
 
 
 def deliverable_partner(lead: Dict[str, Any]) -> str:
